a496b1a40a8ef7637bfc27df53bfd3eaf7edf9a9
yaelsz <yael@szabgab.com>
2016-04-22 15:18:01 +0300

    Added files via upload

--- xo.js
+++ xo.js
@@ -3,6 +3,10 @@ var turn = "x";
  
  function check(btnid) {
     console.log(btnid);
+    console.log(document.getElementById(btnid).value) ;
+    if (document.getElementById(btnid).value !== ""){
+        window.alert("The place is full, you can't write hear ") ;
+    }
     document.getElementById(btnid).value = turn ;
     if ( turn === "o"){
         turn = "x" ;
@@ -11,4 +15,6 @@ var turn = "x";
         turn = "o" ;
     }
     document.getElementById('message').innerHTML = "the turn of " + turn ;   
+ 
  }
+ 
